2c0a7d4b3a276e4b1f343f746a61e7d71f9ae828
bayeng <bayurollins.87@gmail.com>
2023-05-11 12:29:27 +0700

    fixing post API count

--- Assets/_MAIN/Scripts/Networking/PostMethod.cs
+++ Assets/_MAIN/Scripts/Networking/PostMethod.cs
@@ -16,7 +16,7 @@ public class PostMethod : MonoBehaviour
 
     void Start()
     {
-        
+        StartCoroutine(PostData_Coroutine());
     }
 
     public void PostData()
@@ -26,13 +26,24 @@ public class PostMethod : MonoBehaviour
 
     IEnumerator PostData_Coroutine()
     {
-        string uri = "https://my-json-server.typicode.com/typicode/demo/posts";
-
+        string uri = "http://localhost:3000/tes";
+        
         WWWForm form = new WWWForm();
-        form.AddField("Pintu", $"muran");
+        form.AddField("Pintu", Pintu.value);
+        form.AddField("RuangTamuDepan", RuangTamuDepan.value);
+        form.AddField("RuangMakan", RuangMakan.value);
+        form.AddField("HalamanDepan", HalamanDepan.value);
+        form.AddField("Lantai2", Lantai2.value);
+        form.AddField("Balkon", Balkon.value);
+        form.AddField("Kamar", Kamar.value);
+        form.AddField("Toilet", Toilet.value);
 
         using (UnityWebRequest request = UnityWebRequest.Post(uri, form))
         {
+            if (request.isHttpError || request.isNetworkError)
+            {
+                yield return request.error;
+            }
             yield return request.SendWebRequest();
 
         }
